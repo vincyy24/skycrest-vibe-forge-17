8f122e860f89cb44aef31d7f294e5c21c92ff6e9
vincyy24 <vaibhav.k.codes@gmail.com>
2025-10-23 21:19:21 +0530

    update robots.txt

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { copyFileSync } from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -19,4 +20,8 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  async buildEnd() {
+    copyFileSync('./public/robots.txt', './dist/robots.txt');
+  }
+
 }));
